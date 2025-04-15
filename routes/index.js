8e17ebe8b02c1e8dbf14a83ef3da4b2e3e51f6e2
yohaneskohl <kevinyohanesteen@gmail.com>
2025-04-15 23:04:33 +0700

    vercel 2

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,22 @@ const YAML = require("yaml");
 const fs = require("fs");
 const path = require("path");
 
-const User = require("./user.routes")
-const Render = require("./render.routes")
+const User = require("./user.routes");
+const Render = require("./render.routes");
 
+// Endpoint "/" untuk tes backend
+router.get("/", (req, res) => {
+  res.send("<h1>✅ Backend is running</h1>");
+});
 
+// Swagger docs
 const swagger_path = path.resolve(__dirname, "../docs/api-docs.yaml");
 const file = fs.readFileSync(swagger_path, "utf-8");
-
-// API Docs
 const swaggerDocument = YAML.parse(file);
 router.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-// API
-router.use("/api/v1", User)
-router.use("/api/v1", Render)
+// API routes
+router.use("/api/v1", User);
+router.use("/api/v1", Render);
 
 module.exports = router;
